88f575a1683d12a3863c0318e450cc1c0763ef34
vetka10 <146122557+vetka10@users.noreply.github.com>
2023-11-10 12:44:21 +0300

    Семинар4

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿int number = -5;
+﻿Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10 
+// 8 -> 36
 
-int result = number * number;
-
-Console.WriteLine(result);
+ Console.Write("Введите число А: ");
+ int limit = Convert.ToInt32(Console.ReadLine());
+// // Вызов метода
+ Console.WriteLine($"Сумма чисел от 1 до {limit} = {GetSum(limit)}");
+// // тип_метода ИмяМетода (параметр1, параметр2...параметрN)
+ int GetSum (int A)
+ {
+     int sum = 0;
+     // i = 1 - начало, 
+     for (int i = 1; i <= A; i++)
+     {
+         sum += i; // sum = sum + i
+     }
+     return sum;
